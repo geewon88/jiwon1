4bad7acf85db08be51280c2aacd936fa5a97bb61
unknown <geewon88@naver.com>
2022-03-17 10:31:39 +0900

    first commit

---
+++ 과제1.py
@@ -0,0 +1,26 @@
+
+print('Hello Word')
+print('Mary\'s cosmetics')
+print('신씨가 소리질렸다. "도둑이야"')
+print('"C:\Windows"')
+print("안녕하세요. \n만나서\t\t반갑습니다.") #\n은 줄바꿈 \t 탭 간격으로 띄어줌
+movie_rank=['닥터스트레인지', '스플릿', '럭키']
+print(movie_rank)
+movie_rank.append('배트맨')
+print(movie_rank)
+movie_rank.insert(1,'슈퍼맨')
+print(movie_rank)
+nums=[1,2,3,4,5]
+suma=sum(nums)
+print(suma)
+cook=['피자','김밥','만두','양념치킨','족발','피자','김치만두','쫄면','소시지','라면','팥빙수','김치전']
+print(len(cook))
+nums2=[1,2,3,4,5]
+a=len(nums)
+b=sum(nums2)
+print(b/a)s
+print(3==5) #False
+print(3<5) #True
+x=4
+print(1<x<5) #True
+print((3==3) and (4!=3)) #True
